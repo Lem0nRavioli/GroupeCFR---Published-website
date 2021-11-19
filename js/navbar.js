6ad61ddb13d90d2e26615b7011d057b3b1b40e15
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-11-19 12:41:26 +0100

    update js navbar matthieu

--- js/navbar.js
+++ js/navbar.js
@@ -6,7 +6,6 @@ hamburger.addEventListener("click", mobileMenu);
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-    navMenu.style.display = "";
 }
 
 const navLink = document.querySelectorAll(".nav_link");
@@ -16,5 +15,4 @@ navLink.forEach(n => n.addEventListener("click", closeMenu));
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-    navMenu.style.display = "none";
 }
