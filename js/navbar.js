9387c360645a49b3685effc86a122850ad1b1f30
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-11-19 14:40:04 +0100

    update navbar matthieu

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,25 @@
+/*------------------ Variables -------------------------*/
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.getElementById("menu_navbar");
+
+const navLink = document.querySelectorAll(".nav_link");
+
+const navBar = document.getElementById("navbar");
+
+
+/*------------------ Ouverture menu latéral -------------------------*/
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
+    navBar.style.position = "sticky";
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
 
-const navLink = document.querySelectorAll(".nav_link");
+
+/*------------- Fermeture menu latéral click lien menu --------------*/
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
@@ -16,3 +27,13 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+
+/*------------- Fermeture menu latéral on scroll --------------*/
+
+window.addEventListener("scroll", function closeMenuScroll() {
+    closeMenu();
+    setTimeout(() => {
+        navBar.style.position = ""
+    }, 235);
+});
