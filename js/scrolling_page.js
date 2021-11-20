48d6b3f98ffa2afeb41171ee174c069edc768cb2
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-11-20 08:05:58 +0100

    redirect https

---
+++ js/scrolling_page.js
@@ -0,0 +1,81 @@
+
+
+/* ------------ navBar ------------*/
+
+ScrollReveal().reveal('.header', {
+    delay: 300,
+    duration: 600,
+    opacity: 0,
+    reset: false
+    
+});
+
+
+/* --------------- blog -----------*/
+
+    ScrollReveal().reveal('.all_items', {
+            delay: 200,
+            distance: '100rem',
+            duration: 600,
+            easing: 'cubic-bezier(0.5, 0, 0, 1)',
+            interval: 0,
+            opacity: 1,
+            origin: 'left',
+            reset: true
+            
+  
+    });
+
+    /* -------- qui sommes-nous ----------- */
+
+  
+    ScrollReveal().reveal('.text_groupecfr', {
+        delay: 200,
+        distance: '10rem',
+        duration: 600,
+        origin: 'right',
+        reset: true
+        
+
+});
+ScrollReveal().reveal('.this_is_who_i_am', {
+    delay: 200,
+    distance: '10rem',
+    duration: 600,
+    origin: 'left',
+    reset: true
+    
+
+});
+
+ScrollReveal().reveal('.bullet_point', {
+    delay: 200,
+    distance: '10rem',
+    duration: 600,
+    origin: 'left',
+    reset: true
+    
+
+});
+
+
+ScrollReveal().reveal('.text_objectif', {
+    delay: 200,
+    distance: '0rem',
+    duration: 600,
+    origin: 'right',
+    reset: true
+    
+
+});
+
+ScrollReveal().reveal('.team', {
+    delay: 200,
+    distance: '10rem',
+    duration: 600,
+    origin: 'bottom',
+    reset: true
+    
+
+});
+
