19aa014d196f3515b0a6dc0895364591a72f9a9a
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-11-22 14:42:59 +0100

    navbar+partenaires

--- js/navbar.js
+++ js/navbar.js
@@ -13,7 +13,6 @@ const navBar = document.getElementById("navbar");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    navBar.style.position = "sticky";
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
@@ -37,9 +36,6 @@ function closeMenu() {
 
 window.addEventListener("scroll", function closeMenuScroll() {
     closeMenu();
-    setTimeout(() => {
-        navBar.style.position = ""
-    }, 235);
 });
 
 
@@ -78,54 +74,54 @@ window.addEventListener("scroll", function closeMenuScroll() {
 
 // if (window.matchMedia("(max-width: 768px)").matches) {
 
-//     (function (w, d, undefined) {
+    (function (w, d, undefined) {
 
-//         var el_html = d.documentElement,
-//             el_body = d.getElementsByTagName('body')[0],
-//             header = d.getElementById('navbar'),
-//             lastScroll = w.pageYOffset || el_body.scrollTop;
+        var el_html = d.documentElement,
+            el_body = d.getElementsByTagName('body')[0],
+            header = d.getElementById('navbar'),
+            lastScroll = w.pageYOffset || el_body.scrollTop;
 
-//         menuIsStuck = function (triggerElement, wScrollTop, lastScroll) {
+        menuIsStuck = function (triggerElement, wScrollTop, lastScroll) {
 
-//             var wScrollTop = w.pageYOffset || el_body.scrollTop,
-//                 regexp = /(nav\-is\-stuck)/i,
-//                 classFound = el_html.className.match(regexp),
-//                 navHeight = header.offsetHeight,
-//                 bodyRect = el_body.getBoundingClientRect(),
-//                 scrollValue = triggerElement ? triggerElement.getBoundingClientRect().top - bodyRect.top - navHeight : 800,
-//                 scrollValFix = classFound ? scrollValue : scrollValue + navHeight;
+            var wScrollTop = w.pageYOffset || el_body.scrollTop,
+                regexp = /(nav\-is\-stuck)/i,
+                classFound = el_html.className.match(regexp),
+                navHeight = header.offsetHeight,
+                bodyRect = el_body.getBoundingClientRect(),
+                scrollValue = triggerElement ? triggerElement.getBoundingClientRect().top - bodyRect.top - navHeight : 800,
+                scrollValFix = classFound ? scrollValue : scrollValue + navHeight;
 
-//             // si le scroll est d'au moins 600 et
-//             // la class nav-is-stuck n'existe pas sur HTML
-//             if (wScrollTop > scrollValFix && !classFound && wScrollTop < lastScroll) {
-//                 el_html.className = el_html.className + ' nav-is-stuck';
-//                 el_body.style.paddingTop = navHeight + 'px';
-//             }
+            // si le scroll est d'au moins 600 et
+            // la class nav-is-stuck n'existe pas sur HTML
+            if (wScrollTop > scrollValFix && !classFound && wScrollTop < lastScroll) {
+                el_html.className = el_html.className + ' nav-is-stuck';
+                el_body.style.paddingTop = navHeight + 'px';
+            }
 
-//             // si le scroll est inférieur à 2 et
-//             // la class nav-is-stuck existe
-//             if (classFound && wScrollTop > lastScroll) {
-//                 el_html.className = el_html.className.replace(regexp, '');
-//                 el_body.style.paddingTop = '0';
-//             }
-//         },
+            // si le scroll est inférieur à 2 et
+            // la class nav-is-stuck existe
+            if (classFound && wScrollTop > lastScroll) {
+                el_html.className = el_html.className.replace(regexp, '');
+                el_body.style.paddingTop = '0';
+            }
+        },
 
-//             onScrolling = function () {
-//                 // on récupère la valeur du scroll maintenant
-//                 var wScrollTop = w.pageYOffset || el_body.scrollTop;
+            onScrolling = function () {
+                // on récupère la valeur du scroll maintenant
+                var wScrollTop = w.pageYOffset || el_body.scrollTop;
 
-//                 // on ajoute deux arguments, valeurs de scrolls
-//                 menuIsStuck(d.getElementById('main'), wScrollTop, lastScroll);
+                // on ajoute deux arguments, valeurs de scrolls
+                menuIsStuck(d.getElementById('main'), wScrollTop, lastScroll);
 
-//                 // on enregistre notre dernière valeur de scroll
-//                 lastScroll = wScrollTop;
-//             };
+                // on enregistre notre dernière valeur de scroll
+                lastScroll = wScrollTop;
+            };
 
-//         // quand on scroll
-//         w.addEventListener('scroll', function () {
-//             // on exécute la fonction onScrolling()
-//             w.requestAnimationFrame(onScrolling);
-//         });
+        // quand on scroll
+        w.addEventListener('scroll', function () {
+            // on exécute la fonction onScrolling()
+            w.requestAnimationFrame(onScrolling);
+        });
 
-//     }(window, document));
+    }(window, document));
 // }
