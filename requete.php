602b473c83f2908c405251ea3f32a92035b3c6f2
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-11-23 15:28:41 +0100

    php test matthieu

---
+++ requete.php
@@ -0,0 +1,51 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css" />
+    <title>test_requete_CFR</title>
+</head>
+<body>
+    <div>
+        <div class="titre"><h1>Test de requetes</h1></div>
+        
+        <div class="news">
+            <?php
+                try {
+                    $bdd = new PDO('mysql:host=localhost:3306;dbname=ojtb5163_testDB;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                } catch (Exception $e) {
+                    die('Erreur : '.$e->getMessage());
+                }
+                
+                // $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, "%d/%m/%Y à %Hh%imin%ssec") AS date FROM billets ORDER BY date DESC');
+                $req = $bdd->query('SELECT ID, FORMATIONS, METHODE_PEDAGOGIQUE, PUBLIC_VISE, PREREQUIS, OBJECTIFS FROM table_name ORDER BY date DESC');
+
+                // table_name(ID, FORMATIONS, METHODE_PEDAGOGIQUE, PUBLIC_VISE, PREREQUIS, OBJECTIFS)
+            ?>            
+            
+            <?php
+                while ($donnees = $req->fetch()) {
+                if (empty($donnees)) {
+                    echo "La page demandée n'existe pas...";
+                } else {
+            ?>
+                <div class="bloc_message">
+                
+                <?php echo htmlspecialchars($donnees['ID']) . htmlspecialchars($donnees['FORMATIONS']) . htmlspecialchars($donnees['METHODE_PEDAGOGIQUE']) .  htmlspecialchars($donnees['PUBLIC_VISE']) . htmlspecialchars($donnees['PREREQUIS']) . htmlspecialchars($donnees['OBJECTIFS']) . "</br> </br> </br> </br>"; 
+                ?>
+                
+                
+                </div>
+<?php
+        }
+    }
+        $req->closeCursor(); // Fin de requète SQL
+?> 
+
+
+        </div>
+    </div>
+</body>
+</html>
