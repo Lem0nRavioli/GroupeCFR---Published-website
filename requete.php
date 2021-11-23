0eacee4029ac19e2acd6aacefa886d1f6cccc220
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-11-23 15:40:24 +0100

    update php 2

--- requete.php
+++ requete.php
@@ -14,7 +14,8 @@
         <div class="news">
             <?php
                 try {
-                    $bdd = new PDO('mysql:host=localhost:3306;dbname=ojtb5163_testDB;charset=utf8', 'groupecfr_lecture', 'P6ea0c3_!Mxr', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                    $bdd = new PDO('mysql:host=localhost:3306;dbname=ojtb5163_testDB;charset=utf8', '	
+                    ojtb5163_groupecfr_lecture', 'P6ea0c3_!Mxr', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
                 } catch (Exception $e) {
                     die('Erreur : '.$e->getMessage());
                 }
